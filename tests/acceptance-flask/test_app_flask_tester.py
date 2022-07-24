1ffd2a64855ab381e708c5439bd73ec46fc405d0
MMOla92 <mazzieola@hotmail.com>
2022-07-24 20:07:32 +0200

    python reset

--- tests/acceptance-flask/test_app_flask_tester.py
+++ tests/acceptance-flask/test_app_flask_tester.py
@@ -6,7 +6,14 @@ from project import app
 class TestApp(unittest.TestCase):
     token=''
 
-    def test_get_user_products(self):
-        return None
-
+    def test_1_get_user_products(self):
+        # return None
+        tester = app.test_client(self)        
+        print(f"token: {self.token}")
+        headers = {"Authorization": f"Bearer {TestApp.token}"}        
+        response = tester.get('/api/v1/users/1/products', content_type='application/json', headers=headers)       
+        data=json.loads(response.text)        
+        print(f"get_user_products: {data}")
+        self.assertEqual(response.status_code, 200)       
+        self.assertTrue(len(data)>0)
     
