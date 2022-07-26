e701e902e5538b6c5bfe81da48836c176867bfc6
Mazzie <mazzieola@hotmail.com>
2022-07-26 12:25:58 +0200

    acc-test

--- tests/acceptance-flask/test_app_flask_tester.py
+++ tests/acceptance-flask/test_app_flask_tester.py
@@ -5,8 +5,16 @@ from project import app
 
 class TestApp(unittest.TestCase):
     token=''
-
+# In all methods we need to use the token
+# GET METHOD 1 
     def test_get_user_products(self):
+        
         return None
 
+
+    #  Here we get the information of the client
     
+# POST METHOD
+    # Post a new user
+
+
