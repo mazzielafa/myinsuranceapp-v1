f4ec8586f6cf60756018b64c09b43c9cfe4c1cdf
Mazzie <mazzieola@hotmail.com>
2022-07-27 14:06:34 +0200

    flask app

--- tests/acceptance-flask/test_app_flask_tester.py
+++ tests/acceptance-flask/test_app_flask_tester.py
@@ -5,9 +5,7 @@ from project import app
 
 class TestApp(unittest.TestCase):
     token=''
-<<<<<<< HEAD
 
-<<<<<<< HEAD
     def test_1_get_user_products(self):
         # return None
         tester = app.test_client(self)        
@@ -18,24 +16,15 @@ class TestApp(unittest.TestCase):
         print(f"get_user_products: {data}")
         self.assertEqual(response.status_code, 200)       
         self.assertTrue(len(data)>0)
-=======
-=======
+
 # In all methods we need to use the token
 # GET METHOD 1 
->>>>>>> e701e902e5538b6c5bfe81da48836c176867bfc6
+# >>>>>>> e701e902e5538b6c5bfe81da48836c176867bfc6
     def test_get_user_products(self):
         
         return None
-
-<<<<<<< HEAD
->>>>>>> develop
-    
-=======
 
     #  Here we get the information of the client
     
 # POST METHOD
     # Post a new user
-
-
->>>>>>> e701e902e5538b6c5bfe81da48836c176867bfc6
